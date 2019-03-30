72761bc3d78fddf3fb3a3bd36bb13eacf2959322
Aeysha Ibrahim Mahmoud <ayoosh0056@gmail.com>
2019-03-30 12:01:13 +0300

    read me & first file

---
+++ src/firstFile.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+const settings = { timestampsInSnapshots: true };
+
+var config = {
+  apiKey: "AIzaSyBZgLqWjx_XqFSOP_-KcZ3NERtxHhbKFVA",
+  authDomain: "justbeep-0-2.firebaseapp.com",
+  databaseURL: "https://justbeep-0-2.firebaseio.com",
+  projectId: "justbeep-0-2",
+  storageBucket: "justbeep-0-2.appspot.com",
+  messagingSenderId: "1088572560682"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings(settings);
+export default firebase;  
